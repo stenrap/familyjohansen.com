f0617f48f7765c4f7355745b6b104a28598a7322
Rob Johansen <robert_johans22@hotmail.com>
2015-02-12 22:11:48 -0700

    Improve the CSS, add the login help button and the password reset template.

--- public/js/templates/login.js
+++ public/js/templates/login.js
@@ -3,5 +3,5 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 
-buf.push("<form id=\"admin-login-form\"><div class=\"form-group\"><input type=\"text\" name=\"username\" placeholder=\"Username\" autofocus=\"autofocus\" required=\"required\" class=\"form-control\"/></div><div class=\"form-group\"><input type=\"password\" name=\"password\" placeholder=\"Password\" required=\"required\" class=\"form-control\"/></div><button id=\"admin-login-button\" type=\"submit\" data-style=\"slide-down\" class=\"btn btn-lg btn-primary ladda-button\"><span class=\"ladda-label\">Log In</span></button></form>");;return buf.join("");
+buf.push("<form id=\"admin-login-form\"><div class=\"form-group\"><input type=\"text\" name=\"username\" placeholder=\"Username\" autofocus=\"autofocus\" required=\"required\" class=\"form-control\"/></div><div class=\"form-group\"><input type=\"password\" name=\"password\" placeholder=\"Password\" required=\"required\" class=\"form-control\"/></div><button id=\"admin-login-button\" type=\"submit\" data-style=\"slide-down\" class=\"btn btn-lg btn-primary ladda-button\"><span class=\"ladda-label\">Log In</span></button><button id=\"login-help\" type=\"button\" class=\"btn btn-lg btn-primary\">Help</button></form>");;return buf.join("");
 }
